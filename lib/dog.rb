8c4f6104bd555dc1f1d9854eafb47c98f367078c
Vincent Huang <vincent.huang@wework.com>
2018-02-15 23:57:30 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Dog
+
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+end
